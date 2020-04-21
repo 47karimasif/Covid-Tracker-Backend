9890c3f367b5be376ce25a279c2706982092c7e5
Mercer1410 <hs7582@srmist.edu.in>
2020-04-21 21:12:20 +0530

    EJS BOOTSTRAP FONTAWESOME AXIOS installed  ViewsDir  PartialsDir index.ejs created

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
+const axios = require("axios");
+const request = require("request");
+app.set("view engine", "ejs");
 
 app.get("/", (req, res, next) => {
-  res.send("Project-Covid");
+  res.render("index");
 });
 
 const PORT = process.env.PORT || 5000;
