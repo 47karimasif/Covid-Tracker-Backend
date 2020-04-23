f08b64dbf72050a7d97ea61f18a138b5d6caeef0
47karimasif <karimasif98@gmail.com>
2020-04-24 02:13:34 +0530

    updated marker

--- app.js
+++ app.js
@@ -2,42 +2,11 @@ const express = require("express");
 const app = express();
 const axios = require("axios");
 const request = require("request");
-// const covid19 = require("./public/JS/covid19");
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.use(express.static("public"));
-
-// app.get("/cases-json", (req, res) => {
-//   covid19((error, data) => {
-//     if (error) {
-//       return res.send(error);
-//     } else
-//       res.send({
-//         totalconfirmed: data.totalConfirmed,
-//         newconfirmed: data.newConfirmed,
-//         totalrecovered: data.totalRecovered,
-//         newrecovered: data.newRecovered,
-//         totaldeaths: data.totalDeaths,
-//         newdeaths: data.newDeaths,
-//       });
-//   });
-// });
-
-// app.get("/", (req, res, next) => {
-//   request("https://api.covid19api.com/summary", function (
-//     error,
-//     response,
-//     body
-//   ) {
-//     if (!error && response.statusCode == 200) {
-//       var data = JSON.parse(body);
-//       console.log(data);
-//       res.render("index", { data: data });
-//     }
-//   });
-// });
 
 // Updated version
 app.get("/", async (req, res, next) => {
