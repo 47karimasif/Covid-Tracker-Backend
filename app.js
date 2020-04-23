b34245b64564054167da296d6f721ba672c04549
47karimasif <karimasif98@gmail.com>
2020-04-24 02:42:19 +0530

    updated

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const app = express();
 const axios = require("axios");
@@ -5,37 +6,8 @@ const request = require("request");
 // const covid19 = require("./public/JS/covid19");
 
 app.set("view engine", "ejs");
-app.use(express.static("public"));
-
-// app.get("/cases-json", (req, res) => {
-//   covid19((error, data) => {
-//     if (error) {
-//       return res.send(error);
-//     } else
-//       res.send({
-//         totalconfirmed: data.totalConfirmed,
-//         newconfirmed: data.newConfirmed,
-//         totalrecovered: data.totalRecovered,
-//         newrecovered: data.newRecovered,
-//         totaldeaths: data.totalDeaths,
-//         newdeaths: data.newDeaths,
-//       });
-//   });
-// });
-
-// app.get("/", (req, res, next) => {
-//   request("https://api.covid19api.com/summary", function (
-//     error,
-//     response,
-//     body
-//   ) {
-//     if (!error && response.statusCode == 200) {
-//       var data = JSON.parse(body);
-//       console.log(data);
-//       res.render("index", { data: data });
-//     }
-//   });
-// });
+const publicdirectorypath = path.join(__dirname, "/Public");
+app.use(express.static(publicdirectorypath));
 
 // Updated version
 app.get("/", async (req, res, next) => {
