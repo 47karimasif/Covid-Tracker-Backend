5d6708ad1b3bad24deede66df8b4e910daa53513
Mercer1410 <hs7582@srmist.edu.in>
2020-04-23 17:05:59 +0530

    Map Added with Countries  markers, COVID data requested as data

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.get("/", (req, res, next) => {
-  res.render("index");
+  request("https://api.covid19api.com/summary", function(error, response, body){
+        if(!error && response.statusCode == 200){
+            var data = JSON.parse(body);
+            res.render("index", {data: data});
+        }
+    });
 });
 
 const PORT = process.env.PORT || 5000;
