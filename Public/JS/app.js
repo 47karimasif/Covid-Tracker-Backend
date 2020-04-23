c469a4236ee2e4f0a40ee0ec5f30a2ea49bc5a05
47karimasif <karimasif98@gmail.com>
2020-04-23 21:24:28 +0530

    updated code

--- Public/JS/app.js
+++ Public/JS/app.js
@@ -1,15 +1,14 @@
 var d = new Date();
 document.getElementById("date").innerHTML = d.toDateString();
 
-
-fetch('http://localhost:3000/cases-json').then((response)=>{
-    response.json().then((data)=>{
-        console.log(data)
-        document.getElementById("globalcases").innerHTML = data.totalconfirmed
-        document.getElementById("newcases").innerHTML = data.newconfirmed
-        document.getElementById("globalrecovered").innerHTML = data.totalrecovered
-        document.getElementById("newrecovered").innerHTML = data.newrecovered
-        document.getElementById("globaldeath").innerHTML = data.totaldeaths
-        document.getElementById("newdeath").innerHTML = data.newdeaths
-    })
-})
+// fetch("http://localhost:3000/cases-json").then((response) => {
+//   response.json().then((data) => {
+//     console.log(data);
+//     // document.getElementById("globalcases").innerHTML = data.totalconfirmed
+//     document.getElementById("newcases").innerHTML = data.newconfirmed;
+//     document.getElementById("globalrecovered").innerHTML = data.totalrecovered;
+//     document.getElementById("newrecovered").innerHTML = data.newrecovered;
+//     document.getElementById("globaldeath").innerHTML = data.totaldeaths;
+//     document.getElementById("newdeath").innerHTML = data.newdeaths;
+//   });
+// });
--- app.js
+++ app.js
@@ -2,36 +2,55 @@ const express = require("express");
 const app = express();
 const axios = require("axios");
 const request = require("request");
-const covid19 = require ('./public/JS/covid19')
+// const covid19 = require("./public/JS/covid19");
 
 app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 
-app.get("/cases-json", (req, res) => {
-  
-    covid19 ((error,data) =>{
-      if(error){
-        return res.send(error)
-      }
-      else res.send({
-        totalconfirmed : data.totalConfirmed,
-        newconfirmed : data.newConfirmed,
-        totalrecovered: data.totalRecovered,
-        newrecovered : data. newRecovered,
-        totaldeaths: data.totalDeaths,
-        newdeaths : data.newDeaths,
-      })
-    })
-  })
+// app.get("/cases-json", (req, res) => {
+//   covid19((error, data) => {
+//     if (error) {
+//       return res.send(error);
+//     } else
+//       res.send({
+//         totalconfirmed: data.totalConfirmed,
+//         newconfirmed: data.newConfirmed,
+//         totalrecovered: data.totalRecovered,
+//         newrecovered: data.newRecovered,
+//         totaldeaths: data.totalDeaths,
+//         newdeaths: data.newDeaths,
+//       });
+//   });
+// });
 
-app.get("/", (req, res, next) => {
-  request("https://api.covid19api.com/summary", function(error, response, body){
-        if(!error && response.statusCode == 200){
-            var data = JSON.parse(body);
-            res.render("index", {data: data});
-        }
+// app.get("/", (req, res, next) => {
+//   request("https://api.covid19api.com/summary", function (
+//     error,
+//     response,
+//     body
+//   ) {
+//     if (!error && response.statusCode == 200) {
+//       var data = JSON.parse(body);
+//       console.log(data);
+//       res.render("index", { data: data });
+//     }
+//   });
+// });
+
+// Updated version
+app.get("/", async (req, res, next) => {
+  try {
+    const response = await axios.get("https://api.covid19api.com/summary");
+    res.render("index", {
+      data: {
+        global: response.data.Global,
+        Countries: response.data.Countries,
+      },
     });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 const PORT = process.env.PORT || 3000;
