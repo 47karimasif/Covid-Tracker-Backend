e97e3d3635adfd2f8587c593acfe48417f6e3126
Arsh <Codetheon@users.noreply.github.com>
2020-04-23 20:00:06 +0530

    Global Data Shown

--- Public/JS/app.js
+++ Public/JS/app.js
@@ -1,8 +1,15 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoiYWRhcnNoYWdyd2FsIiwiYSI6ImNrN3BuN2FoNTA4bGEzaXFkcjJvYXQ0ZnAifQ.IaCVUsnZqH77-agcveZKEw';
-var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/dark-v10',
-    zoom:1,
-    center:[28.7041,77.1025]
-});
-map.addControl(new mapboxgl.NavigationControl());
+var d = new Date();
+document.getElementById("date").innerHTML = d.toDateString();
+
+
+fetch('http://localhost:3000/cases-json').then((response)=>{
+    response.json().then((data)=>{
+        console.log(data)
+        document.getElementById("globalcases").innerHTML = data.totalconfirmed
+        document.getElementById("newcases").innerHTML = data.newconfirmed
+        document.getElementById("globalrecovered").innerHTML = data.totalrecovered
+        document.getElementById("newrecovered").innerHTML = data.newrecovered
+        document.getElementById("globaldeath").innerHTML = data.totaldeaths
+        document.getElementById("newdeath").innerHTML = data.newdeaths
+    })
+})
--- app.js
+++ app.js
@@ -2,18 +2,37 @@ const express = require("express");
 const app = express();
 const axios = require("axios");
 const request = require("request");
+const covid19 = require ('./public/JS/covid19')
 
 app.set("view engine", "ejs");
 
-app.use(express.static('Public'))
+app.use(express.static("public"));
+
+app.get("/cases-json", (req, res) => {
+  
+    covid19 ((error,data) =>{
+      if(error){
+        return res.send(error)
+      }
+      else res.send({
+        totalconfirmed : data.totalConfirmed,
+        newconfirmed : data.newConfirmed,
+        totalrecovered: data.totalRecovered,
+        newrecovered : data. newRecovered,
+        totaldeaths: data.totalDeaths,
+        newdeaths : data.newDeaths,
+      })
+    })
+  })
 
 app.get("/", (req, res, next) => {
-  res.render("index");
+  request("https://api.covid19api.com/summary", function(error, response, body){
+        if(!error && response.statusCode == 200){
+            var data = JSON.parse(body);
+            res.render("index", {data: data});
+        }
+    });
 });
 
-app.get("/maps", (req, res, next) => {
-  res.render("maps");
-});
-
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`App running on ${PORT}`));
