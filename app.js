a75ffc1ce4d79b9ff413db2f7bf94887f8120b19
Mercer1410 <hs7582@srmist.edu.in>
2020-04-24 22:31:50 +0530

    Global News extracted as news and passed into index

--- app.js
+++ app.js
@@ -12,13 +12,19 @@ app.use(express.static(publicdirectorypath));
 // Updated version
 app.get("/", async (req, res, next) => {
   try {
-    const response = await axios.get("https://api.covid19api.com/summary");
-    res.render("index", {
-      data: {
-        global: response.data.Global,
-        Countries: response.data.Countries,
-      },
-    });
+    const one =await axios.get("https://api.covid19api.com/summary");
+    const two =await axios.get("http://newsapi.org/v2/everything?q=covid&sortBy=publishedAt&language=en&apiKey=2ce620556cce40d6923ca8ee1d3374fe");
+    axios.all([one, two]).then(axios.spread((...responses) => {
+      const response1 = responses[0];
+      const response2 = responses[1];
+      res.render("index", {
+        data: {
+          global: response1.data.Global,
+          Countries: response1.data.Countries,
+        },
+        news: response2.data
+      });
+    }));    
   } catch (err) {
     console.log(err);
   }
