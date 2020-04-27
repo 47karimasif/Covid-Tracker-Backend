dd368f7418eedf8dc5d06cfbc76b5b43d27596b3
Yadvi Bhalla <60046892+yadvi12@users.noreply.github.com>
2020-04-27 16:39:29 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ const path = require("path");
 const express = require("express");
 const app = express();
 const axios = require("axios");
+const bodyParser = require("body-parser");
 const request = require("request");
 // const covid19 = require("./public/JS/covid19");
 
 app.set("view engine", "ejs");
-const publicdirectorypath = path.join(__dirname, "/Public");
+const publicdirectorypath = path.join(__dirname, "/public");
 app.use(express.static(publicdirectorypath));
+app.use(bodyParser.urlencoded({extended: true}));
 
+
+var display = [];
 // Updated version
 app.get("/", async (req, res, next) => {
   try {
@@ -18,19 +22,25 @@ app.get("/", async (req, res, next) => {
         global: response.data.Global,
         Countries: response.data.Countries,
       },
+      disp: display
     });
   } catch (err) {
     console.log(err);
   }
 });
 
+app.post("/country", function(req,res){
+  var dt = req.body;
+  display.pop();
+  display.push(dt);
+  res.redirect("/");
+});
+
 // News Route
 app.get("/global-news", async (req, res, next) => {
   try {
-    const response = await axios.get(
-      "http://newsapi.org/v2/everything?q=covid&sortBy=publishedAt&language=en&apiKey=2ce620556cce40d6923ca8ee1d3374fe"
-    );
-    res.render("news", { news: response.data });
+    const response = await axios.get("http://newsapi.org/v2/everything?q=covid&sortBy=publishedAt&language=en&apiKey=2ce620556cce40d6923ca8ee1d3374fe");
+    res.render("news", {news: response.data});
   } catch (err) {
     console.log(err);
   }
